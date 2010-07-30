670543f2a7be1458a432c043a7726e4cf2446bb0
John Dugan <jdugan@coroutine.com>
2010-07-29 22:43:17 -0500

    Second crack at rails3 generators.

--- lib/generators/kenny_dialoggins/kenny_dialoggins_generator.rb
+++ lib/generators/kenny_dialoggins/kenny_dialoggins_generator.rb
@@ -1,16 +1,17 @@
+require "rails/generators"
+
+
 class KennyDialogginsGenerator < Rails::Generator::Base
 
-  # This method establishes the path to the templates directory.
+  # This call establishes the path to the templates directory.
   #
-  def self.source_root
-    @source_root ||= File.expand_path('../templates', __FILE__)
-  end
+  source_root File.expand_path('../templates', __FILE__)
 
 
   # This method copies stylesheet and javascript files to the 
   # corresponding public directories.
   #
-  def copy_assets
+  def generate_assets
     copy_file "kenny_dialoggins.js",  "public/javascripts/kenny_dialoggins.js"
     copy_file "kenny_dialoggins.css", "public/stylesheets/kenny_dialoggins.css"
   end
